a8f466b83dcac547fbfa54fdb8f65227c2e6ff70
Vivibg <155114409+Vivibg@users.noreply.github.com>
2025-03-23 23:14:12 -0300

    Add files via upload

---
+++ TareaModulo6.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tarea Modulo 6
+# # **Ciencia de Datos**
+
+# ## **Introducción**
+# ## Este cuaderno forma parte del proyecto final del curso. Aquí aplicaremos conceptos básicos de ciencia de datos utilizando celdas Markdown y celdas de código en JupyterLite Notebook.
+
+# ## Ejercicio 4 - Lenguajes de ciencia de datos
+# ## **Lenguaje de cienca de datos:**
+# ### 1. Python
+# ### 2. R
+# ### 3. SQL
+# ### 4. Java
+#  
+
+# ## Ejercicio 5 - Bibliotecas de ciencia de datos
+# ## **Some of the commonly used libraries by Data Scientists include:**
+# ### 1. Pandas
+# ### 2. NumPy
+# ### 3. Matplotlib
+# ### 4. Scikit-learn
+# ### 5. TensorFlow
+
+# ## Ejercicio 6 - Tabla de herramientas de Ciencia de Datos
+# 
+# | Herramienta       | Descripción breve                         |
+# |-------------------|-------------------------------------------|
+# | Jupyter Notebook  | Entorno interactivo para código y texto   |
+# | RStudio           | Entorno de desarrollo para R              |
+# | GitHub            | Plataforma de control de versiones        |
+# | Apache Spark      | Motor de análisis de grandes datos        |
+# 
+
+# ## Ejercicio 7 - Ejemplos de expresiones aritméticas
+# ### *A continuación se muestran algunas expresiones aritméticas básicas, como suma, resta, multiplicación y división:*
+# 
+
+# In[12]:
+
+
+# Ejercicio 8 - Multiplicación y suma
+a = 5
+b = 10
+resultado = a * b + 20
+print("Resultado:", resultado)
+
+
+# In[13]:
+
+
+# Ejercicio 9 - Conversión de minutos a horas
+minutos = 200
+horas = minutos / 60
+print("Horas:", horas)
+
+
+# In[14]:
+
+
+# Ejercicio 10 - Lista de objetivos del curso
+objetivos = ["Aprender fundamentos de Ciencia de Datos", "Usar Jupyter Notebooks", "Aplicar Python en proyectos"]
+for objetivo in objetivos:
+    print("-", objetivo)
+
+
+# ## Ejercicio 11 - Autor
+# ### Author
+# # Vivi
